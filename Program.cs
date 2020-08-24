67687fdf0cfb7185b927ad95f77b322751c74b09
alanAval <alan@avaltech.com.br>
2020-08-24 19:21:26 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace c_
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            int requestCount = 0;
+            var list = new List<Task<HttpResponseMessage>>();
+            while (true) { 
+                list.Add(client.GetAsync("http://nupreco.com/"));
+                Console.WriteLine((++requestCount).ToString());
+                if (requestCount == 5)
+                    break;
+            }
+
+            while (list.Where(a => !a.IsCompleted).Any());
+
+            Console.WriteLine("Fim do ataque");
+
+            await Task.Delay(100);
+        }
+    }
+}
